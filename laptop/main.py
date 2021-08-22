a6befdff8282f2e407e5370fda03b4c7edd5d8a9
loloToster <lolo.toster4@gmail.com>
2021-08-22 13:27:25 +0200

    add: python oops

--- laptop/main.py
+++ laptop/main.py
@@ -5,9 +5,6 @@ import os
 WS_SERVER = "http://localhost:8080"
 SECRET = secrets.WS_SECRET
 
-# path = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
-# with open(path + "/" + "secret.txt") as f:
-#    SECRET = f.readline()
 
 sio = socketio.Client()
 
@@ -17,4 +14,4 @@ def connect():
     print("I'm connected!")
 
 
-sio.connect(WS_SERVER)
+sio.connect(WS_SERVER, auth={"role": "python", "key": SECRET})
