09e838f9e48f9b2fc5f8c9c96261ffdf248afb56
loloToster <lolo.toster4@gmail.com>
2021-08-25 13:24:52 +0200

    moved passport setup to /config/passport-setup.js

--- server/app.js
+++ server/app.js
@@ -9,45 +9,12 @@ const express = require("express"),
     io = require("socket.io")(server) // , { cors: { origin: "*" } }
 
 const passport = require("passport"),
-    googleStrategy = require("passport-google-oauth20")
+    passportSetup = require(path.join(__dirname, "/config/passport-setup"))
 
 const mongoose = require("mongoose"),
     User = require(path.join(__dirname, "/models/user-model"))
 
 const cookieSession = require("cookie-session")
-
-
-passport.serializeUser((user, done) => {
-    done(null, user.googleId)
-})
-
-passport.deserializeUser(async (id, done) => {
-    user = await User.findOne({ googleId: id })
-    done(null, user)
-})
-
-passport.use(
-    new googleStrategy({
-        callbackURL: "/redirect",
-        clientID: process.env.GOOGLE_AUTH_ID,
-        clientSecret: process.env.GOOGLE_AUTH_SECRET
-    }, async (accessToken, refreshToken, profile, done) => {
-        let currentUser = await User.findOne({ googleId: profile.id })
-        if (currentUser) {
-            console.log("User is: " + currentUser.name)
-            done(null, currentUser)
-        } else {
-            let newUser = await new User({
-                name: profile.displayName,
-                googleId: profile.id,
-                email: profile._json.email,
-                thumbnail: profile._json.picture
-            }).save()
-            console.log("New user: " + profile.displayName)
-            done(null, newUser)
-        }
-    })
-)
 
 const PY_SECRET = process.env.PY_SECRET
 const COOKIE_SECRET = process.env.COOKIE_SECRET
---
+++ server/config/passport-setup.js
@@ -0,0 +1,36 @@
+const path = require("path"),
+    passport = require("passport"),
+    googleStrategy = require("passport-google-oauth20"),
+    User = require(path.join(__dirname, "/../models/user-model"))
+
+passport.serializeUser((user, done) => {
+    done(null, user.googleId)
+})
+
+passport.deserializeUser(async (id, done) => {
+    user = await User.findOne({ googleId: id })
+    done(null, user)
+})
+
+passport.use(
+    new googleStrategy({
+        callbackURL: "/redirect",
+        clientID: process.env.GOOGLE_AUTH_ID,
+        clientSecret: process.env.GOOGLE_AUTH_SECRET
+    }, async (accessToken, refreshToken, profile, done) => {
+        let currentUser = await User.findOne({ googleId: profile.id })
+        if (currentUser) {
+            console.log("User is: " + currentUser.name)
+            done(null, currentUser)
+        } else {
+            let newUser = await new User({
+                name: profile.displayName,
+                googleId: profile.id,
+                email: profile._json.email,
+                thumbnail: profile._json.picture
+            }).save()
+            console.log("New user: " + profile.displayName)
+            done(null, newUser)
+        }
+    })
+)
