d61dc18aed3ba1fcfb26fa399f0b363e436b8a14
loloToster <lolo.toster4@gmail.com>
2021-09-01 23:24:11 +0200

    add onBreakStart and onBreakStop events

--- laptop/main.py
+++ laptop/main.py
@@ -1,17 +1,84 @@
 import secrets
-import socketio
 import os
+import datetime
+import socketio
+from pymongo import MongoClient
 
-WS_SERVER = "http://localhost:8080"
+BREAKS = [
+    ("09:00", "09:10"),
+    ("09:55", "10:05"),
+    ("10:50", "11:00"),
+    ("11:45", "11:55"),
+    ("12:40", "13:00"),
+    ("13:45", "14:05"),
+    ("14:50", "14:55"),
+]
+
+WS_SERVER = "http://radiowezel.herokuapp.com/"
+MONGO_URL = secrets.MONGO_URL
 SECRET = secrets.WS_SECRET
 
+cluster = MongoClient(MONGO_URL)
+db = cluster["learning"]
+voteElements = db["voteelements"]
+# videos = [
+#     {"ytid": i["video"]["ytid"], "votes": i["votes"]} for i in voteElements.find({})
+# ]
+# print(videos)
 
-sio = socketio.Client()
+
+socket = socketio.Client(
+    reconnection_attempts=3,
+    reconnection_delay=5,
+)
+breakCurrently = False
 
 
-@sio.event
+def clock():
+    global breakCurrently
+    while True:
+        previousBreakCurrently = breakCurrently
+        time = datetime.datetime.now()
+        for BREAK in BREAKS:
+            breakStart = {
+                "hour": int(BREAK[0].split(":")[0]),
+                "minute": int(BREAK[0].split(":")[1]),
+            }
+            breakStop = {
+                "hour": int(BREAK[1].split(":")[0]),
+                "minute": int(BREAK[1].split(":")[1]),
+            }
+            if (
+                time.hour > breakStart["hour"]
+                and time.hour < breakStop["hour"]
+                and time.minute > breakStart["minute"]
+                and time.minute < breakStop["minute"]
+            ):
+                breakCurrently = True
+                break
+        else:
+            breakCurrently = False
+        if breakCurrently != previousBreakCurrently:
+            print((time.hour, time.minute))
+            if breakCurrently:
+                onBreakStart()
+            else:
+                onBreakStop()
+        socket.sleep(1)
+
+
+def onBreakStart():
+    print("break start")
+
+
+def onBreakStop():
+    print("break stop")
+
+
+@socket.event
 def connect():
     print("I'm connected!")
+    socket.start_background_task(clock)
 
 
-sio.connect(WS_SERVER, auth={"role": "python", "key": SECRET})
+socket.connect(WS_SERVER, auth={"role": "python", "key": SECRET})
---
+++ laptop/secrets.example.py
@@ -0,0 +1,2 @@
+WS_SECRET = "secret key to for ws connection"
+MONGO_URL = "url to mongodb database"
