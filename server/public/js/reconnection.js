9b88224113803acbd1e919040276ab06bbc20431
loloToster <lolo.toster4@gmail.com>
2021-11-05 17:18:59 +0100

    added better reconnection page

--- server/public/js/reconnection.js
+++ server/public/js/reconnection.js
@@ -1,12 +1,23 @@
+let disconnectMsg = document.createElement("div")
+disconnectMsg.classList.add("disconnectedMsg")
+let disconnectImg = document.createElement("img")
+disconnectImg.src = "/images/broken-vinyl.png"
+disconnectMsg.appendChild(disconnectImg)
+let disconnectedMsgText = document.createElement("div")
+disconnectedMsgText.innerText = "Rozłączono z serwerem :/"
+disconnectMsg.appendChild(disconnectedMsgText)
+
 socket.on("disconnect", () => {
     console.log("disconnected")
-    document.body.innerHTML = "<div>disconnected</div>"
+    document.body.innerHTML = ""
+    console.log(disconnectMsg)
+    document.body.appendChild(disconnectMsg)
     reconnect()
 })
 
 function reconnect() {
     if (socket.disconnected)
-        setTimeout(() => reconnect(), 10)
+        setTimeout(reconnect, 10)
     else
         window.location.reload()
 }
