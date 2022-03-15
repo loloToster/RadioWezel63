714fc2e996650413f8f9ca50e2fa1a948a110080
loloToster <lolo.toster4@gmail.com>
2022-03-15 21:20:21 +0100

    💄 added info for empty html lists

--- server/routes/submit.js
+++ server/routes/submit.js
@@ -52,8 +52,8 @@ module.exports = (io, logger) => {
         }
 
         res.render("submit", {
-            results: searchResults,
-            searching: Boolean(query)
+            query,
+            results: searchResults
         })
     })
 
